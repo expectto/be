2ee9d1020b060f4e50df6a4f54db3b29860c0561
Evghenii <not.pennys.code@gmail.com>
2023-11-04 00:46:28 +0200

    update comments

--- types/be_matcher.go
+++ types/be_matcher.go
@@ -1,22 +1,26 @@
 package types
 
+// GomockMatcher stands for matcher that fits Gomock library
+// We intentionally don't use Matcher interface from gomock library
+// To avoid dependency on gomock library
+type GomockMatcher interface {
+	Matches(x any) bool
+	String() string
+}
+
+// GomegaMatcher stands for matcher that fits Gomega library
+// We intentionally don't use GomegaMatcher interface from gomega library.
+// Although Gomega lib is anyway used as a dependency in other parts of `be`
+// And will probably remain as a dep for now
 type GomegaMatcher interface {
 	Match(actual interface{}) (success bool, err error)
 	FailureMessage(actual interface{}) (message string)
 	NegatedFailureMessage(actual interface{}) (message string)
 }
 
-type GomockMatcher interface {
-	Matches(x any) bool
-	String() string
-}
-
-// BeMatcher currently stands for matcher that fits both Gomega and Gomock libraries
-// todo: that's a draft yet, and a subject to be changed:
-//
-//	we want to be more flexible: not to have gomock as a MUST probably
-//	but let to have testify be suported as well
-//	Though `gomega` will probably remain as a MUST dependency
+// BeMatcher is main matcher interface for `be` library
+// It combines all types of Matchers that `be` supports:
+// Currently it's GomegaMatcher and GomockMatcher.
 type BeMatcher interface {
 	GomegaMatcher
 	GomockMatcher
