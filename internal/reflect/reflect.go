b8ce937e6857bd6ace21d8c6399ecb1efff9cc1f
Evghenii <not.pennys.code@gmail.com>
2023-11-03 11:19:29 +0200

    reflect.LengthOf

--- internal/reflect/reflect.go
+++ internal/reflect/reflect.go
@@ -19,3 +19,16 @@ func IndirectDeep(v reflect.Value) reflect.Value {
 	}
 	return v
 }
+
+// LengthOf returns length of a given type
+func LengthOf(a any) (int, bool) {
+	if a == nil {
+		return 0, false
+	}
+	switch reflect.TypeOf(a).Kind() {
+	case reflect.Map, reflect.Array, reflect.String, reflect.Chan, reflect.Slice:
+		return reflect.ValueOf(a).Len(), true
+	default:
+		return 0, false
+	}
+}
