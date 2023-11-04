c61fabc1694803e5d1ef9c9b3d8c065f9e112a75
Evghenii <not.pennys.code@gmail.com>
2023-11-04 23:31:38 +0200

    not matcher

---
+++ internal/psi/matchers/not.go
@@ -0,0 +1,38 @@
+package psiMatchers
+
+import "github.com/expectto/be/types"
+
+type NotMatcher struct {
+	Matcher         types.BeMatcher
+	lastActualValue any
+}
+
+func (m *NotMatcher) Match(actual interface{}) (bool, error) {
+	success, err := m.Matcher.Match(actual)
+	if err != nil {
+		return false, err
+	}
+	m.lastActualValue = actual
+	return !success, nil
+}
+
+func (m *NotMatcher) FailureMessage(actual interface{}) (message string) {
+	return m.Matcher.NegatedFailureMessage(actual) // works beautifully
+}
+
+func (m *NotMatcher) NegatedFailureMessage(actual interface{}) (message string) {
+	return m.Matcher.FailureMessage(actual) // works beautifully
+}
+
+func (m *NotMatcher) Matches(actual any) bool {
+	res, _ := m.Match(actual)
+	return res
+}
+
+// Todo: inaccurate behavior should be fixed
+func (m *NotMatcher) String() string {
+	mes := m.FailureMessage(m.lastActualValue)
+	return mes
+}
+
+// todo: MatchMayChangeInTheFuture
