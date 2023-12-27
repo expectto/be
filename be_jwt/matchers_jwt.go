1d397febee21c44aaca80d76980e54795e9d5587
Jack <not.pennys.code@gmail.com>
2023-12-28 01:08:03 +0200

    be_jwt matchers & examples

--- be_jwt/matchers_jwt.go
+++ be_jwt/matchers_jwt.go
@@ -1,3 +1,6 @@
+// Package be_jwt provides Be matchers for handling JSON Web Tokens (JWT).
+// It includes matchers for transforming and validating JWT tokens.
+// Matchers corresponds to specific golang jwt implementation: https://github.com/golang-jwt/jwt/v5
 package be_jwt
 
 import (
@@ -7,90 +10,110 @@ import (
 	"github.com/expectto/be/internal/psi_matchers"
 	"github.com/expectto/be/types"
 	"github.com/golang-jwt/jwt/v5"
-	"github.com/onsi/gomega/gcustom"
+	"github.com/onsi/gomega"
+	"strings"
 )
 
-var TransformJwtFromString = func(input string) (*jwt.Token, error) {
-	return jwt.Parse(input, func(token *jwt.Token) (interface{}, error) {
-		return []string{""}, nil
-	})
-}
-
-var TransformSignedJwtFromString = func(secret string) func(string) (*jwt.Token, error) {
-	return func(input string) (*jwt.Token, error) {
-		return jwt.Parse(input, func(token *jwt.Token) (interface{}, error) {
-			return []string{secret}, nil
+// TransformSignedJwtFromString returns a transform function (string->*jwt.Token) for a given secret.
+var TransformSignedJwtFromString = func(secret string) func(string) any {
+	return func(input string) any {
+		parsed, err := jwt.Parse(input, func(token *jwt.Token) (any, error) {
+			return []byte(secret), nil
 		})
+		if err != nil {
+			return NewTransformError(fmt.Errorf("to parse jwt token (with secret=%s): %w", secret, err), input)
+		}
+
+		return parsed
 	}
 }
 
-// Token matches actual value to be a valid *jwt.Token corresponding to given inputs
+// TransformJwtFromString is a transform function (string->*jwt.Token) without a secret.
+// It parses the input string as a JWT and returns the resulting *jwt.Token.
+var TransformJwtFromString = func(input string) any {
+	p := jwt.NewParser()
+
+	t, parts, err := p.ParseUnverified(input, jwt.MapClaims{})
+	if err != nil {
+		return NewTransformError(err, input)
+	}
+
+	t.Signature, err = p.DecodeSegment(parts[2])
+	if err != nil {
+		return NewTransformError(fmt.Errorf("corrupted signature part: %w", err), input)
+	}
+
+	return t
+}
+
+// Token matches the actual value to be a valid *jwt.Token corresponding to given inputs.
 // Possible inputs:
-// 1. Nil args -> so actual value MUST be any valid *jwt.Token
-// 2. Single arg <string>. Actual value MUST be a *jwt.Token, whose .String() compared against args[0]
-// 3. Single arg <*jwt.Token>. Actual value MUST be a *jwt.Token
-// 4. List of Omega/Gomock/Psi matchers, that are applied to *jwt.Token object
-//   - TransformJwtFromString/TransformSignedJwtFromString(secret) transforms can be given as first argument,
-//     so string->*jwt.Token transform is applied
+// 1. No args -> the actual value MUST be any valid *jwt.Token.
+// 2. Single arg <string>. The actual value MUST be a *jwt.Token, whose .String() is compared against args[0].
+// 3. Single arg <*jwt.Token>. The actual value MUST be a *jwt.Token.
+// 4. List of Omega/Gomock/Psi matchers that are applied to *jwt.Token object.
+//   - TransformJwtFromString/TransformSignedJwtFromString(secret) transforms can be given as the first argument,
+//     so the string->*jwt.Token transform is applied.
 func Token(args ...any) types.BeMatcher {
 	if len(args) == 0 {
-		return psi_matchers.NewUrlFieldMatcher("", "", nil)
+		return psi_matchers.NewJwtTokenMatcher("", nil)
 	}
 
+	// Given single string arg
 	if cast.IsString(args[0], cast.AllowCustomTypes(), cast.AllowPointers()) {
 		if len(args) != 1 {
 			panic("string arg must be a single arg")
 		}
 
-		return Psi(gcustom.MakeMatcher(func(actual interface{}) (bool, error) {
-			token, ok := actual.(*jwt.Token)
-			if !ok {
-				return false, fmt.Errorf("actual must be a *jwt.Token")
-			}
-			return token.Raw == cast.AsString(args[0]), nil
-		}))
-
-		// match given string to whole url
-		//return psi_matchers.NewUrlFieldMatcher("Url", "", func(u *url.URL) any {
-		//	return u.String()
-		//}, gomega.Equal(args[0]))
+		// match given string to raw token
+		return psi_matchers.NewJwtTokenMatcher("Raw", func(t *jwt.Token) any {
+			return t.Raw
+		}, gomega.Equal(args[0]))
 	}
 
 	return Psi(args...)
 }
 
-func SignedVia(secret string) types.BeMatcher {
-	return Psi(gcustom.MakeMatcher(func(actual interface{}) (bool, error) {
-		token, ok := actual.(*jwt.Token)
-		if !ok {
-			return false, fmt.Errorf("actual must be a *jwt.Token")
-		}
-
-		// actual token may be not signed, let's re-sign it with given signature
-
-		//token.Raw
-
-		return token.Valid, nil
-	}))
+// Valid checks if the jwt.Token is valid.
+func Valid() types.BeMatcher {
+	return psi_matchers.NewJwtTokenMatcher(
+		"Valid",
+		func(u *jwt.Token) any { return u.Valid },
+		gomega.BeTrue(),
+	)
 }
 
-func BeingValid() types.BeMatcher {
-	return Psi(gcustom.MakeMatcher(func(actual interface{}) (bool, error) {
-		token, ok := actual.(*jwt.Token)
-		if !ok {
-			return false, fmt.Errorf("actual must be a *jwt.Token")
-		}
-
-		return token.Valid, nil
-	}))
-}
-
+// HavingClaims checks if the jwt.Token matches given claims.
 func HavingClaims(args ...any) types.BeMatcher {
-	return Psi(gcustom.MakeMatcher(func(actual interface{}) (bool, error) {
-		token, ok := actual.(*jwt.Token)
-		if !ok {
-			return false, fmt.Errorf("actual must be a *jwt.Token")
-		}
-		return Psi(args...).Match(token.Claims)
-	}))
+	return psi_matchers.NewJwtTokenMatcher(
+		"Claims",
+		func(u *jwt.Token) any { return u.Claims },
+		Psi(args...),
+	)
+}
+
+// HavingMethodAlg checks if the jwt.Token has a method and algorithm matching given arguments.
+func HavingMethodAlg(args ...any) types.BeMatcher {
+	return psi_matchers.NewJwtTokenMatcher(
+		"Method.Alg()",
+		func(u *jwt.Token) any { return u.Method.Alg() },
+		Psi(args...),
+	)
+}
+
+// SignedVia matches a valid & signed token (with a given secret).
+// Token(TransformSignedJwtFromString(secret), Valid()) is the same as
+// Token(TransformJwtFromString, SignedVia(secret)).
+// It's useful when you already have matching against a secret-less token,
+// and need the secret only for one specific matching.
+func SignedVia(secret string) types.BeMatcher {
+	return psi_matchers.NewJwtTokenMatcher(
+		"Method.Verify()",
+		func(u *jwt.Token) any {
+			// text is parts[0]+parts[1]
+			text := strings.Join(strings.Split(u.Raw, ".")[0:2], ".")
+			return u.Method.Verify(text, u.Signature, []byte(secret))
+		},
+		gomega.BeNil(),
+	)
 }
