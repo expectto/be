32054547b63194647c0e78ab45ca433c93eb95c9
Jack <not.pennys.code@gmail.com>
2024-01-06 02:09:59 +0200

    more aliases

--- matchers.go
+++ matchers.go
@@ -2,12 +2,19 @@ package be
 
 import (
 	"github.com/expectto/be/be_http"
+	"github.com/expectto/be/be_jwt"
+	"github.com/expectto/be/be_strings"
 	"github.com/expectto/be/be_url"
 )
 
-// HttpRequest is a type alias for be_http.Request
-// So be.HttpRequest can be used instead of be_http.Request
+// HttpRequest is an alias for be_http.Request matcher
 var HttpRequest = be_http.Request
 
-// URL is a type alias for be_url.URL
+// URL is an alias for be_url.URL matcher
 var URL = be_url.URL
+
+// JwtToken is an alias for be_jwt.Token matcher
+var JwtToken = be_jwt.Token
+
+// StringAsTemplate is an alias for be_strings.MatchTemplate matcher
+var StringAsTemplate = be_strings.MatchTemplate
